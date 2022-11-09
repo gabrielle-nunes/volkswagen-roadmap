a341de7599a6bf1c3a4c2f117ed0fb4f764998fc
Arthur <71472433+Arthur-devi@users.noreply.github.com>
2022-11-09 19:53:21 -0300

    .
    .

--- Backend/src/main/java/br/com/roadmap/volkswagen/controller/InovacaoController.java
+++ Backend/src/main/java/br/com/roadmap/volkswagen/controller/InovacaoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import br.com.roadmap.volkswagen.entities.Hg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
